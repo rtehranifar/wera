318dfb4bd07221922c293031207b16d317d8f08c
rzt <rzt101@gmail.com>
2017-10-04 21:49:42 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,291 @@
+
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 40.6944, lng: -73.9213},
+		zoom: 11,
+		styles: [
+		{
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#f5f5f5"
+			}
+			]
+		},
+		{
+			"elementType": "labels.icon",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#616161"
+			}
+			]
+		},
+		{
+			"elementType": "labels.text.stroke",
+			"stylers": [
+			{
+				"color": "#f5f5f5"
+			}
+			]
+		},
+		{
+			"featureType": "administrative.country",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "administrative.land_parcel",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#bdbdbd"
+			}
+			]
+		},
+		{
+			"featureType": "administrative.locality",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "administrative.neighborhood",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#eeeeee"
+			}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#757575"
+			}
+			]
+		},
+		{
+			"featureType": "poi.attraction",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.business",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.government",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.medical",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#e5e5e5"
+			}
+			]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "labels.text",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.park",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#9e9e9e"
+			}
+			]
+		},
+		{
+			"featureType": "poi.place_of_worship",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.school",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "poi.sports_complex",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#ffffff"
+			}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#757575"
+			}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#dadada"
+			}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#616161"
+			}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#9e9e9e"
+			}
+			]
+		},
+		{
+			"featureType": "transit.line",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#e5e5e5"
+			}
+			]
+		},
+		{
+			"featureType": "transit.station",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#eeeeee"
+			}
+			]
+		},
+		{
+			"featureType": "transit.station.bus",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#c9c9c9"
+			}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#9e9e9e"
+			}
+			]
+		}
+		]
+
+	});
+}
+
+map.data.loadGeoJson('data/nyc_neighborhoods.geojson');
+
+
+
+
+
+
+
+
+// define global variables
+	//map
+	//geojson
+// create evenhandlers
+	//switch colors
+	//load aside with ethnic group
+	// click on ethnic group
+		// hit api
+		// show restaurants on map
+		// show restaurants on aside
+
+// on load
+	// initialize map
+	// load geojson
